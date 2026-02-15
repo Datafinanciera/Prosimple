67d2654ea212569668dcfe11ea8f9f22559eb3c2
Datafinanciera <alpipro@gmail.com>
2026-02-14 23:14:33 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot conectado como {bot.user}')
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send("Hola 👋 Soy tu bot!")
+
+bot.run(TOKEN)
 
